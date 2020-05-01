acb8b43e95d0812279a7c9bf84d51c8e946a2af4
Ahmed Bibars <42313457+AhmedBibars@users.noreply.github.com>
2020-05-01 16:55:36 +0200

    Update MapCamLocalization.h

--- MapCamLocalization.h
+++ MapCamLocalization.h
@@ -1,3 +1,8 @@
+/* This class Takes a specific image file, and try to find its real printed-copy in real-time video recived from a camera moves in 3D world.
+Regardless any changes in scale and oriatation of the printed-image.
+Also you can determin specific Target-point (2D pixel location) in the target Image, and the class will ougment the real-time video with a mark over this location inside the printed-photo.
+*/
+
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/gpu/gpu.hpp"
